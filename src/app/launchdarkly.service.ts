214887138876002a7eb1800471a18f8840b04498
dineshk092020 <dineshk092020@gmail.com>
2021-09-10 12:40:52 +0200

    1 file modified

--- src/app/launchdarkly.service.ts
+++ src/app/launchdarkly.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Component, Output, EventEmitter } from '@angular/core';
+import {
+  Injectable,
+  Component,
+  Output,
+  Input,
+  EventEmitter
+} from '@angular/core';
 import * as LDClient from 'launchdarkly-js-client-sdk';
 
 @Injectable()
@@ -8,7 +14,8 @@ export class LaunchdarklyService {
   public flag;
   message: string = 'Hola Test!';
 
-  @Output() messageEvent = new EventEmitter<string>();
+  //@Output() messageEvent = new EventEmitter<string>();
+  @Output() messageEvent: EventEmitter<string> = new EventEmitter();
 
   constructor() {
     this.user = { key: '613196e03eb897267017a321' };
@@ -19,22 +26,18 @@ export class LaunchdarklyService {
       console.log('on ready ');
       if (this.client.variation('helloworld', false)) {
         console.log('ready - Flag is on ');
+        this.message = 'ready - Flag is on';
         this.flag = true;
       } else {
         console.log('ready - Flag is off');
+        this.message = 'ready - Flag is off';
         this.flag = false;
       }
       this.messageEvent.emit(this.message);
     });
 
-    this.client.on('change', flags => {
-      console.log('in change');
-      if (flags['helloworld'] !== undefined) {
-        console.log('in change - hw01');
-        console.log(flags['helloworld']);
-        console.log('in change - hw2');
-        this.flag = flags['helloworld'];
-      }
+    this.client.on('change', function(settings) {
+      console.log('flags changed:', settings);
     });
   }
 }
